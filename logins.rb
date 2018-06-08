d04f30842dc9e3d6203e3bbe00588de90abc0174
AlyssaEdwards13 <edwardsalyssa83@gmail.com>
2018-06-08 20:35:37 +0000

    automatically backed up by learn

--- logins.rb
+++ logins.rb
@@ -9,6 +9,21 @@ class Users
   end 
 end 
 
+class Users2
+  
+  def name2=(username)
+    @the_username = username 
+  end 
+  
+  def name2
+    @the_username
+  end 
+end 
+
 jessica = Users.new 
 jessica.name = "Jessica"
 puts jessica.name
+
+void_ivy = Users2.new
+void_ivy.name2 = "Void_ivy"
+puts Void_ivy.name2
