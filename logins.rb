d6a2ffe3f5aa4f1cfc5c1ee5913cd846d038c5d0
AlyssaEdwards13 <edwardsalyssa83@gmail.com>
2018-06-08 20:36:23 +0000

    automatically backed up by learn

--- logins.rb
+++ logins.rb
@@ -1,29 +1,29 @@
-class Users 
+# class Users 
   
-  def name=(username)
-    @the_username = username
-  end 
+#   def name=(username)
+#     @the_username = username
+#   end 
   
-  def name
-    @the_username
-  end 
-end 
+#   def name
+#     @the_username
+#   end 
+# end 
 
-class Users2
+# class Users2
   
-  def name2=(username)
-    @the_username = username 
-  end 
+#   def name2=(username)
+#     @the_username = username 
+#   end 
   
-  def name2
-    @the_username
-  end 
-end 
+#   def name2
+#     @the_username
+#   end 
+# end 
 
-jessica = Users.new 
-jessica.name = "Jessica"
-puts jessica.name
+# jessica = Users.new 
+# jessica.name = "Jessica"
+# puts jessica.name
 
-void_ivy = Users2.new
-void_ivy.name2 = "Void_ivy"
-puts void_ivy.name2
+# void_ivy = Users2.new
+# void_ivy.name2 = "Void_ivy"
+# puts void_ivy.name2
